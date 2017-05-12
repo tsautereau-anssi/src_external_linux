6882d8c97feece87795cfbff8e8a5c42e22a6f0c
Daniel Micay <danielmicay@gmail.com>
2017-05-12 03:22:00 -0400

    mark kernel_set_to_readonly as __ro_after_init
    This change was extracted from PaX where it's part of KERNEXEC.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -1257,7 +1257,7 @@ void __init mem_init(void)
 	mem_init_print_info(NULL);
 }
 
-int kernel_set_to_readonly;
+int kernel_set_to_readonly __ro_after_init;
 
 void mark_rodata_ro(void)
 {
@@ -1270,9 +1270,8 @@ void mark_rodata_ro(void)
 
 	printk(KERN_INFO "Write protecting the kernel read-only data: %luk\n",
 	       (end - start) >> 10);
-	set_memory_ro(start, (end - start) >> PAGE_SHIFT);
-
 	kernel_set_to_readonly = 1;
+	set_memory_ro(start, (end - start) >> PAGE_SHIFT);
 
 	/*
 	 * The rodata/data/bss/brk section (but not the kernel text!)
