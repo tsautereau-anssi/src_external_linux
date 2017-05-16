f18d658b5c9df653c94e12ee25c7847836cfda01
Daniel Micay <danielmicay@gmail.com>
2017-05-16 00:59:48 -0400

    PaX shadow cr4 sanity check (essentially a revert)
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    [thibaut.sautereau@ssi.gouv.fr: Adapt to cpu_tlbstate moved out-of-line]
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -399,6 +399,8 @@ void cr4_update_irqsoff(unsigned long set, unsigned long clear)
 {
 	unsigned long newval, cr4 = this_cpu_read(cpu_tlbstate.cr4);
 
+	BUG_ON(cr4 != __read_cr4());
+
 	lockdep_assert_irqs_disabled();
 
 	newval = (cr4 & ~clear) | set;
